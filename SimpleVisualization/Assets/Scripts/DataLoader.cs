f1f0d23615a6a5d55df90081722a4d19ce8df449
Eric <luv.ilho@gmail.com>
2018-04-08 20:02:07 -0700

    03. Adding DataLoader cs file

---
+++ SimpleVisualization/Assets/Scripts/DataLoader.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataLoader : MonoBehaviour {
+    public DataVisualizer Visualizer;
+	void Start () {
+        TextAsset popData = Resources.Load("popdata") as TextAsset;
+        string jsonString = popData.ToString();
+    
+        PopulationData[] populations =  JsonHelper.getJsonArray<PopulationData> (jsonString);
+        Visualizer.CreateMeshes(populations);
+    }
+}
+public class JsonHelper
+{
+    public static T[] getJsonArray<T>(string json)
+    {
+        string newJson = "{ \"array\": " + json + "}";
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>> (newJson);
+        return wrapper.array;
+    }
+ 
+    [System.Serializable]
+    private class Wrapper<T>
+    {
+        public T[] array;
+    }
+}
+
+[System.Serializable]
+public class PopulationData
+{
+    public string city;
+    public string city_ascii;
+    public float lat;
+    public float lng;
+    public float pop;
+
+    public string country;
+    public string iso2;
+    public string iso3;
+    public string province;
+}
